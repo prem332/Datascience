cbd60c35924b06ff4dfd5a6e2a309b3b21eedf68
prem332 <premkumarkale7@gmail.com>
2024-10-07 15:22:24 +0530

    House Price Prediction system project

---
+++ House_Price_Prediction_System/app/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5596fdea8dcb37cea848276ef001d8fe6f134c82e70c8ad1e14f932f1cc58e55
+size 3239
---
+++ House_Price_Prediction_System/app/model_training.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0720b8360f1c34e77348cff1be2e86390573a710ac6aa2d58f2e5acc6e5a8df7
+size 5865
